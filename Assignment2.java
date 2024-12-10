b327140cbfafd01dbd8cbba44b187bc6a7453ab7
shahdmagdi <168036431+shahdmagdi@users.noreply.github.com>
2024-12-10 13:35:01 +0200

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment2;
+import java.util.Scanner;
+/**
+ *
+ * @author Ard Al Jood
+ */
+public class Assignment2 {
+ public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number <= 3) {
+            return true;
+        }
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+
+        int i = 5;
+        while (i * i <= number) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+            i += 6;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    
+    }
+}
